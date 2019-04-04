9299d7be728ea7b3a2f8b88d9c070f76f1bb83b1
Romain niam0r <niamor@mm.st>
2019-04-04 17:51:53 +0100

    add one liner

--- javascript/moveZeros.js
+++ javascript/moveZeros.js
@@ -4,3 +4,7 @@ const moveZeros = (arr) => {
   arr.forEach(el => el === 0 ? zeros.push(el) : result.push(el));
   return [...result, ...zeros]
 }
+
+const moveZeros = (arr) => {
+  return arr.filter(el => el !== 0).concat(arr.filter(el => el === 0));
+}
