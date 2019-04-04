31df4fd54ed59ca953935343fbb582d91d7b3739
Romain niam0r <niamor@mm.st>
2019-04-04 17:14:49 +0100

    lint and refactor

--- javascript/mexican_wave.js
+++ javascript/mexican_wave.js
@@ -1,38 +1,30 @@
-function wave(str){
+function wave(str) {
   const result = [];
   if (str === '') return [];
 
   const leadingSpaces = str.match(/^\s/) ? str.match(/^\s/).join() : '';
-
   const lettersCount = str.replace(/\s/g, '').length;
+  let spacesCount = 0;
 
-  const characters = str.split();
-
-  let spacesCount = 0
-
-  for (let i = 0; i < lettersCount; i++) {
-    let index = i;
-
-    let word = str.replace(/^\s/, '');
+  for (let i = 0; i < (lettersCount + spacesCount); i += 1) {
+    const word = str.replace(/^\s/, '');
 
     if (word[i] === ' ') {
       i += (1 + spacesCount);
       spacesCount += 1;
     }
 
-    const waved =
-      leadingSpaces +
-      word.substring(0, i) +
-      word.charAt(i).toUpperCase() +
-
-      word.substring(i + 1);
+    const waved = leadingSpaces
+      + word.substring(0, i)
+      + word.charAt(i).toUpperCase()
+      + word.substring(i + 1);
 
     result.push(waved);
   }
 
-  console.log(result)
   return result;
 }
+
 
 wave("hello")
 // ["Hello", "hEllo", "heLlo", "helLo", "hellO"];
